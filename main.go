08e9e44d310ca571bcfbe31f6fa0ec4ca7b9397b
Sabbir Ahmed <alreadysabbir@live.com>
2017-02-05 06:02:00 +0600

    Added board info, wireless endpoints; refactoring 🤣

--- main.go
+++ main.go
@@ -1,33 +1,41 @@
 package main
 
 import (
-	"encoding/json"
-	"gopkg.in/gin-gonic/gin.v1"
-	"net/http"
-	"wrtapi/utils"
+    "encoding/json"
+    "gopkg.in/gin-gonic/gin.v1"
+    "net/http"
+    "wrtapi/utils"
 )
 
 func ubusResponse(s func() ([]byte, error), res *gin.Context) {
-	stat, err := s()
-	if err != nil {
-		res.JSON(http.StatusNotImplemented, gin.H{"error": "ubus failed"})
-	} else {
-		var parsed interface{}
-		json.Unmarshal(stat, &parsed)
-		res.JSON(http.StatusOK, parsed)
-	}
+    stat, err := s()
+    if err != nil {
+        res.JSON(http.StatusNotImplemented, gin.H{"error": "ubus failed"})
+    } else {
+        var parsed interface{}
+        json.Unmarshal(stat, &parsed)
+        res.JSON(http.StatusOK, parsed)
+    }
 }
 
 func main() {
-	router := gin.Default()
+    router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		ubusResponse(utils.UbusCall.Info, c)
-	})
+    router.GET("/", func(c *gin.Context) {
+        ubusResponse(utils.UbusCall.BoardInfo, c)
+    })
 
-	router.GET("/wan", func(c *gin.Context) {
-		ubusResponse(utils.UbusCall.WanStatus, c)
-	})
+    router.GET("/wan", func(c *gin.Context) {
+        ubusResponse(utils.UbusCall.WanStatus, c)
+    })
 
-	router.Run() // 0.0.0.0:8080
+    router.GET("/wlan", func(c *gin.Context) {
+        ubusResponse(utils.UbusCall.WirelessStatus, c)
+    })
+
+    router.GET("/system", func(c *gin.Context) {
+        ubusResponse(utils.UbusCall.Info, c)
+    })
+
+    router.Run() // 0.0.0.0:8080
 }
--- utils/system.go
+++ utils/system.go
@@ -1,21 +1,30 @@
 package utils
 
 import (
-	"os/exec"
-	"strings"
+    "os/exec"
+    "strings"
 )
 
 type Ubus func(arg string) ([]byte, error)
 
 var UbusCall = Ubus(func(arg string) ([]byte, error) {
-	args := strings.Split(arg, " ")
-	return exec.Command("ubus", args...).Output()
+    args := strings.Split(arg, " ")
+    args = append([]string{"-S", "call"}, args...)
+    return exec.Command("ubus", args...).Output()
 })
 
 func (u Ubus) Info() ([]byte, error) {
-	return u("-S call system info")
+    return u("system info")
+}
+
+func (u Ubus) BoardInfo() ([]byte, error) {
+    return u("system board")
 }
 
 func (u Ubus) WanStatus() ([]byte, error) {
-	return u("-S call network.interface.wan status")
+    return u("network.interface.wan status")
+}
+
+func (u Ubus) WirelessStatus() ([]byte, error) {
+    return u("network.wireless status")
 }
