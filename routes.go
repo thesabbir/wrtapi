40b98866b03046d21d7a1dc2579e8ea474275d12
Sabbir Ahmed <alreadysabbir@live.com>
2017-02-06 06:38:55 +0600

    refactor response module

--- routes.go
+++ routes.go
@@ -8,36 +8,36 @@ import (
 
 func RouterInit() http.Handler {
     router := httprouter.New()
-    router.GET("/", utils.Handler(func(w http.ResponseWriter) {
+    router.GET("/", Handler(func(w http.ResponseWriter) {
         w.Header().Set("Content-Type", "application/json")
         w.Write([]byte(`{"wrtapi": "0.1"}`))
     }));
-    router.GET("/system", utils.Handler(func(w http.ResponseWriter) {
-        utils.UbusResponse(utils.UbusCall.Info, w)
+    router.GET("/system", Handler(func(w http.ResponseWriter) {
+        UbusResponse(utils.UbusCall.Info, w)
     }))
-    router.GET("/system/board", utils.Handler(func(w http.ResponseWriter) {
-        utils.UbusResponse(utils.UbusCall.BoardInfo, w)
+    router.GET("/system/board", Handler(func(w http.ResponseWriter) {
+        UbusResponse(utils.UbusCall.BoardInfo, w)
     }))
-    router.GET("/system/services", utils.Handler(func(w http.ResponseWriter) {
-        utils.UbusResponse(utils.UbusCall.ServiceList, w)
+    router.GET("/system/services", Handler(func(w http.ResponseWriter) {
+        UbusResponse(utils.UbusCall.ServiceList, w)
     }))
-    router.GET("/network", utils.Handler(func(w http.ResponseWriter) {
-        utils.UbusResponse(utils.UbusCall.NetworkConfig, w)
+    router.GET("/network", Handler(func(w http.ResponseWriter) {
+        UbusResponse(utils.UbusCall.NetworkConfig, w)
     }))
-    router.GET("/network/interfaces", utils.Handler(func(w http.ResponseWriter) {
-        utils.UbusResponse(utils.UbusCall.InterfacesList, w)
+    router.GET("/network/interfaces", Handler(func(w http.ResponseWriter) {
+        UbusResponse(utils.UbusCall.InterfacesList, w)
     }))
-    router.GET("/network/interfaces/wan", utils.Handler(func(w http.ResponseWriter) {
-        utils.UbusResponse(utils.UbusCall.WanStatus, w)
+    router.GET("/network/interfaces/wan", Handler(func(w http.ResponseWriter) {
+        UbusResponse(utils.UbusCall.WanStatus, w)
     }))
-    router.GET("/network/interfaces/lan", utils.Handler(func(w http.ResponseWriter) {
-        utils.UbusResponse(utils.UbusCall.LanStatus, w)
+    router.GET("/network/interfaces/lan", Handler(func(w http.ResponseWriter) {
+        UbusResponse(utils.UbusCall.LanStatus, w)
     }))
-    router.GET("/wlan", utils.Handler(func(w http.ResponseWriter) {
-        utils.UbusResponse(utils.UbusCall.WirelessStatus, w)
+    router.GET("/wlan", Handler(func(w http.ResponseWriter) {
+        UbusResponse(utils.UbusCall.WirelessStatus, w)
     }))
-    router.GET("/wlan/clients", utils.Handler(func(w http.ResponseWriter) {
-        utils.UbusResponse(utils.UbusCall.WlanClients, w)
+    router.GET("/wlan/clients", Handler(func(w http.ResponseWriter) {
+        UbusResponse(utils.UbusCall.WlanClients, w)
     }))
     return router
 }
